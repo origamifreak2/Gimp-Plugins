00ff13a8760ac85b4feb86f2bba97363d8ebc3ac
origamifreak2 <origamifreak2@gmail.com>
2024-12-28 14:51:22 -0500

    getting rid of underscores before _image and _drawable

--- sixteen_by_nine.py
+++ sixteen_by_nine.py
@@ -3,15 +3,15 @@
 from gimpfu import *
 
 # convert image to 16 x 9 with blurred background
-def sixteen_by_nine(_image, _drawable):
+def sixteen_by_nine(image, drawable):
 
     # create duplicate layer to use as foreground
-    forground = _drawable.copy()
-    _image.add_layer(forground, 0)
+    forground = drawable.copy()
+    image.add_layer(forground, 0)
 
     # get canvas width and height
-    canvas_width = pdb.gimp_image_width(_image)
-    canvas_height = pdb.gimp_image_height(_image)
+    canvas_width = pdb.gimp_image_width(image)
+    canvas_height = pdb.gimp_image_height(image)
 
     # get 16:9 width based on height
     width_16_9 = int(round((float(canvas_height) / 9) * 16))
@@ -38,13 +38,13 @@ def sixteen_by_nine(_image, _drawable):
         bg_height = canvas_height * bg_scale
 
     # resize canvas and center foreground
-    pdb.gimp_image_resize(_image, canvas_width, canvas_height, off_x, off_y)
+    pdb.gimp_image_resize(image, canvas_width, canvas_height, off_x, off_y)
 
     # resize background
-    pdb.gimp_layer_scale(_drawable, bg_width, bg_height, True)
+    pdb.gimp_layer_scale(drawable, bg_width, bg_height, True)
 
     # blur background
-    pdb.plug_in_gauss(_image, _drawable, 90, 90, 0)
+    pdb.plug_in_gauss(image, drawable, 90, 90, 0)
 
 
 register(
@@ -57,7 +57,7 @@ register(
           "SixteenByNine",
           "*",
           [
-              (PF_IMAGE, "image", "Input image", None),
+              (PFimage, "image", "Input image", None),
               (PF_DRAWABLE, "drawable", "Input drawable", None),
           ],
           [],
